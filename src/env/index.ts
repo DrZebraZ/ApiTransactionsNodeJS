29b03411cfd1b6544ff96b2e6ce2c144ff1d8087
Luis Zebra <luis.andres@smartnx.com>
2023-07-13 00:32:34 -0300

    fix: changed from database_path to migrations_path in env

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  DATABASE_PATH: z.string(),
+  MIGRATIONS_PATH: z.string(),
   PORT: z.coerce.number().default(3333),
 })
 
